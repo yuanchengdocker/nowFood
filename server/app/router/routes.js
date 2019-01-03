ec6345c6581d5f08c93b32e1558f81ae37b41fd9
yuancheng <yuancheng@inboyu.com>
2019-01-03 17:54:30 +0800

    api

--- server/app/router/routes.js
+++ server/app/router/routes.js
@@ -6,22 +6,22 @@ function addMapping(router, mapping) {
     for (var url in mapping) {
         if (url.startsWith('GET ')) {
             var path = url.substring(4);
-            path += api
+            path = api + path
             router.get(path, mapping[url]);
             console.log(`register URL mapping: GET ${path}`);
         } else if (url.startsWith('POST ')) {
             var path = url.substring(5);
-            path += api
+            path = api + path
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
         } else if (url.startsWith('PUT ')) {
             var path = url.substring(4);
-            path += api
+            path = api + path
             router.put(path, mapping[url]);
             console.log(`register URL mapping: PUT ${path}`);
         } else if (url.startsWith('DELETE ')) {
             var path = url.substring(7);
-            path += api
+            path = api + path
             router.del(path, mapping[url]);
             console.log(`register URL mapping: DELETE ${path}`);
         } else {
