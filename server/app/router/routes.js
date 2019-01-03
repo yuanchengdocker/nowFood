726e6a0e02395ef308be66564f244174cac30611
yuancheng <yuancheng@inboyu.com>
2019-01-03 17:51:53 +0800

    api

--- server/app/router/routes.js
+++ server/app/router/routes.js
@@ -2,21 +2,26 @@ const router = require('koa-router')();
 import index from '../controller/indexController'
 
 function addMapping(router, mapping) {
+    const api = '/now'
     for (var url in mapping) {
         if (url.startsWith('GET ')) {
             var path = url.substring(4);
+            path += api
             router.get(path, mapping[url]);
             console.log(`register URL mapping: GET ${path}`);
         } else if (url.startsWith('POST ')) {
             var path = url.substring(5);
+            path += api
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
         } else if (url.startsWith('PUT ')) {
             var path = url.substring(4);
+            path += api
             router.put(path, mapping[url]);
             console.log(`register URL mapping: PUT ${path}`);
         } else if (url.startsWith('DELETE ')) {
             var path = url.substring(7);
+            path += api
             router.del(path, mapping[url]);
             console.log(`register URL mapping: DELETE ${path}`);
         } else {
